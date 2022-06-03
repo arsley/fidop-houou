6840c2def6500914b82bd10056ca9059b7cd6937
Aoshi Fujioka <blue20will@gmail.com>
2022-06-04 02:06:33 +0900

    feat: create members by scaffold

---
+++ test/controllers/members_controller_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+class MembersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @member = members(:one)
+  end
+
+  test "should get index" do
+    get members_url, as: :json
+    assert_response :success
+  end
+
+  test "should create member" do
+    params = {
+      member: {
+        discord_id: '0' * 18,
+        name: 'Temporary name'
+      }
+    }
+    assert_difference("Member.count") do
+      post members_url, params:, as: :json
+    end
+
+    assert_response :created
+  end
+
+  test "should show member" do
+    get member_url(@member), as: :json
+    assert_response :success
+  end
+
+  test "should update member" do
+    params = {
+      member: {
+        id: @member.id,
+        name: 'updated name'
+      }
+    }
+    patch member_url(@member), params:, as: :json
+    assert_response :success
+  end
+
+  test "should destroy member" do
+    assert_difference("Member.count", -1) do
+      delete member_url(@member), as: :json
+    end
+
+    assert_response :no_content
+  end
+end
