0d5059231dc46bdd8de10be1d42aab1d30a102f2
Aoshi Fujioka <blue20will@gmail.com>
2022-06-05 02:08:15 +0900

    fix: allow CORS on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,13 +45,11 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      # fidop-yakuman uses 3001 on yarn dev
+      origins 'http://localhost:3001'
+      resource '*', headers: :any, methods: %i[get post put patch delete options head], credentials: true
+    end
+  end
 end
