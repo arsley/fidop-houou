f9437f534191a2540f3a8a0d7dc0e85cf0dc8f8c
Aoshi Fujioka <blue20will@gmail.com>
2022-06-23 20:00:28 +0900

    feat: add references to access_token for administrator

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -4,6 +4,8 @@ class AccessToken < ApplicationRecord
   has_secure_token :token, length: 64
   has_secure_token :auth_token, length: 64
 
+  belongs_to :administrator
+
   def self.valid?(jwt)
     return false if jwt.blank?
 
@@ -15,7 +17,7 @@ class AccessToken < ApplicationRecord
   end
 
   def to_jwt
-    payload = { token:, auth_token: }
+    payload = { administrator_id:, token:, auth_token: }
     JWT.encode(payload, Rails.application.credentials.secret_key_base, 'HS512')
   end
 end
